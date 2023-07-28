e39675a29fde27251dea524066d2135f20666c5f
elody-ramirez <elody.ramirez@gmail.com>
2023-07-28 15:17:52 -0400

    Added react router dom and intiial browser router and paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+
+import { Home, About, Services, Gallery, Asked, ServiceAreas } from "./components"
+
 function App() {
 
   return (
     <>
-      <h1>Alfredo&aposs Auto Body</h1>
+      <Routes>
+        <Route path="/" element= {<Home />}/>
+        <Route path="/about" element= {<About />}/>
+        <Route path="/services" element= {<Services />}/>
+        <Route path="/gallery" element= {<Gallery />}/>
+        <Route path="/asked" element= {<Asked />}/>
+        <Route path="/serviceAreas" element= {<ServiceAreas />}/>
+      </Routes>
+      <h1>Alfredo&apos;s Auto Body</h1>
     </>
   )
 }
